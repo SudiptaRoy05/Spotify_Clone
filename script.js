36eb775e8e8b5fe0b02e424816c3e093e1c37b99
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-04-12 13:12:19 +0600

    Update JS and Design

--- script.js
+++ script.js
@@ -1,47 +1,58 @@
-console.log("JS Start");
+console.log("JS STARTS");
 
 async function getSongs() {
-    try {
-        let response = await fetch("http://127.0.0.1:5500/songs/");
-        let html = await response.text();
+    let a = await fetch("http://127.0.0.1:5500/songs/");
+    let response = await a.text();
 
-        let div = document.createElement("div");
-        div.innerHTML = html;
+    // console.log(response);
 
-        let as = div.getElementsByTagName("a");
+    let div = document.createElement("div");
+    div.innerHTML = response;
 
-        let songs = [];
 
-        for (let index = 0; index < as.length; index++) {
-            const element = as[index];
-
-            if (element.href.endsWith(".mp3")) {
-                songs.push(element.href); // Push the href, not the element itself
-            }
+    let as = div.getElementsByTagName("a");
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith("mp3")) {
+            songs.push(element.href.split("/songs/")[1])
         }
-        return songs;
-    } catch (error) {
-        console.error("Error fetching songs:", error);
-        return [];
     }
+
+    return songs
+
+
 }
+getSongs();
 
 async function main() {
-    try {
-        // Get the list of songs
-        let songs = await getSongs();
-        console.log(songs);
+    //get all song
+    let songs = await getSongs()
+    // console.log(songs);
+// Show all the songs of the playlist
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
 
-        // Play the first song
-        if (songs.length > 0) {
-            var audio = new Audio(songs[0]);
-            audio.play(); // Correctly invoking the play method
-        } else {
-            console.log("No songs found");
-        }
-    } catch (error) {
-        console.error("Error in main function:", error);
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li>
+        <img class="invert" src="logo/music.svg" alt="">
+        <div class="info">
+            <div class="songName">
+            ${song.replaceAll("%20"," ")}
+            </div>
+            <div class="songArt">
+                Sudipta
+            </div>
+        </div>
+       <div class="playNow">
+        <span>Play Now</span>
+        <img class="invert" src="logo/play.svg" alt="">
+       </div>
+    </li>
+`;
     }
+
+
+    
 }
 
-main();
+main()
